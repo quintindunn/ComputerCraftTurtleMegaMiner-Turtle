50e394893c7c87a8075dd9a5158a4af24727ac76
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2025-12-22 16:38:35 -0500

    Add updater

---
+++ update.lua
@@ -0,0 +1,6 @@
+shell.run("delete /excavator/")
+shell.run("delete /tools/")
+shell.run("delete /startup.lua")
+shell.run("delete /setup.lua")
+shell.run("delete /api/*.lua")
+shell.run("/installer.lua")
