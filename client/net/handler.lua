74915d159e70f4d9e9ffeb4b427aaa6c080d545e
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2025-12-22 21:36:07 -0500

    Added move down

--- client/net/handler.lua
+++ client/net/handler.lua
@@ -2,24 +2,35 @@ local movement = require("/api/movement")
 local netCore = require("/client/net/core")
 local excavator = require("/excavator/groupMine")
 local utils = require("/api/utils")
+local logger = require("/api/logger").getLogger("Handler")
 
 function moveForwardHandler()
+    logger("Moving forward")
     movement.forward()
 end
 
 function moveUpHandler()
+    logger("Moving up")
     movement.up()
 end
 
+function moveDownHandler()
+    logger("Moving down")
+    movement.down()
+end
+
 function moveBackHandler()
+    logger("Moving back")
     movement.back()
 end
 
 function iterateHandler()
+    logger("Running iteration")
     excavator.iteration()
 end
 
 function dumpUpHandler()
+    logger("Dumping inventory")
     utils.dumpInventory()
 end
 
@@ -41,6 +52,9 @@ function handle(msg)
         netCore.sendState()
     elseif msgType == "moveUp" then
         moveUpHandler(msg)
+        netCore.sendState()
+    elseif msgType == "moveDown" then
+        moveDownHandler(msg)
         netCore.sendState()
     elseif msgType == "moveBack" then
         moveBackHandler(msg)
